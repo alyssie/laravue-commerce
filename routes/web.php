ee4933ad27b12c3b5d69a4c9b94696c20e3e57c3
Alyssa Mae Caluya <caluyaalyssamae@gmail.com>
2024-08-29 07:01:30 +0800

    Lesson 1: Set up Multiple Authentication and Admin Dashboard User Interface

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminAuthController;
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// User Routes
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -35,4 +28,20 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// End User Routes
+
+
+// Admin Routes
+Route::group(['prefix' => 'lvc-admin', 'middleware' => 'redirectAdmin'], function(){
+    Route::get('/login', [AdminAuthController::class, 'showLoginForm'])->name('admin.login');
+    Route::post('/login', [AdminAuthController::class, 'login'])->name('admin.login.post');
+    Route::post('/logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
+});
+
+Route::middleware(['auth', 'admin'])->prefix('lvc-admin')->group(function(){
+    Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+});
+
+
+// End Admin Routes
 require __DIR__.'/auth.php';
